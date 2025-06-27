864e324bf5e2155a8d76749d4b78ca605397080f
Bruno Costa <bruno.costa@inspired.consulting>
2025-06-27 12:48:18 +0200

    feat: base commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="flex flex-col items-center justify-center text-3xl font-bold underline">{{ msg }}</h1>
+
+  <div class="flex flex-col items-center justify-center">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+
+    <p>
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+      starter
+    </p>
+    <p>
+      Learn more about IDE Support for Vue in the
+      <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up
+        Guide</a>.
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  </div>
+</template>
